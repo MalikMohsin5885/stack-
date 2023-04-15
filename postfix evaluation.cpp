3c5774bb8b496d4b8b1d7c97c1fc63f34af96c41
MalikMohsin5885 <malikmohsin8239@gmail.com>
2023-04-15 11:06:45 +0500

    second commit
    now works for multiple digit also

--- postfix evaluation.cpp
+++ postfix evaluation.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <cstring>
 using namespace std;
 
 struct Stack
@@ -9,7 +10,6 @@ struct Stack
 
 void Push(int value)
 {
-	cout<<"to be pushed=>	"<<value<<endl;
 	if(myStack.Top == (sizeof(myStack.Array)/sizeof(char)))
 		cout<<"Stack Overflow"<<endl;
 	else
@@ -70,13 +70,33 @@ void postfixEvaluation(char (&Equation)[50] , int total)
 			}
 				break;
 			
-			default :
+			case '%' :
 			{
-				Push(Equation[currentIndex] - '0');
-				cout<<"Pushed	"<<Equation[currentIndex]<<endl;
+				int operand_2 = Pop();
+				int operand_1 = Pop();
+				Push(operand_1 / operand_2);
 			}
 				break;
+			case ' ' :
+			{
+				//ignore spaces
+			}
+				break;
+			default :
+			{
+//				Push(Equation[currentIndex] - '0');
+//				cout<<"Pushed	"<<Equation[currentIndex]<<endl;
 
+				int number = 0;
+				while(isdigit(Equation[currentIndex]))
+				{
+					number = number * 10 + (Equation[currentIndex] - '0');
+					currentIndex++;
+				}
+				Push(number);
+				currentIndex--;
+			}
+				break;
 		}
 	}
 	
@@ -87,9 +107,11 @@ int main()
 	myStack.Top = -1;
 	char i = 0 , Equation[50];
 	cout<<"Enter Equation : ";
-	while(cin>>Equation[i])
-		i++;
-	postfixEvaluation(Equation,i);
+//	while(cin>>Equation[i])
+//		i++;
+	cin.getline(Equation , 50);
+	
+	postfixEvaluation(Equation,strlen(Equation));
 	cout<<"top	"<<myStack.Top<<endl;
 	cout<<"result	"<<myStack.Array[myStack.Top];
 }
